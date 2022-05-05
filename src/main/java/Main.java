497306170b1e88f1de9b7f246629e942ddfcd7dd
seth <22097951+Seth2482@users.noreply.github.com>
2022-05-05 23:35:47 +0800

    Apply Material UI && Add Gson library

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,25 @@
+import mdlaf.MaterialLookAndFeel;
+import mdlaf.themes.JMarsDarkTheme;
 import view.WelcomeFrame;
 
 import javax.swing.*;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+        initializeUI();
         SwingUtilities.invokeLater(() -> {
             WelcomeFrame welcomeFrame = new WelcomeFrame();
             welcomeFrame.setVisible(true);
         });
+    }
+
+    public static void initializeUI(){
+        try {
+            Locale.setDefault(new Locale("en"));
+            UIManager.setLookAndFeel(new MaterialLookAndFeel());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
     }
 }
